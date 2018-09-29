d98295c41c2b43c2c98c123ca2df630648a9106e
SANDEEP\padala <sandeep.dm@live.com>
2018-09-29 13:56:39 +0530

    Initial project commit

---
+++ src/test/java/test/TestRunner.java
@@ -0,0 +1,44 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestRunner {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void before() {
+
+		System.setProperty("webdriver.edge.driver", "E:\\Job Stuff\\TestngMAven\\src\\test\\resources\\MicrosoftWebDriver.exe");
+		driver = new EdgeDriver();
+	}
+	@Test
+	public void Scenario1() {
+		driver.get("https://www.phptravels.net/");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"s2id_autogen8\"]/a/span[1]"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys("SOME DATA");
+		actions.build().perform();
+	}
+	
+	@AfterTest
+	public void after() {
+		driver.close();
+	}
+}
